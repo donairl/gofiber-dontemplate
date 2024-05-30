df19c7664b66b39213f2b5503c9a1f53d3f64394
donny airlangga <donairl@suryasoft.com>
2024-05-30 17:13:45 +0700

    auth

--- handlers/auth.go
+++ handlers/auth.go
@@ -3,5 +3,31 @@ package handlers
 import "github.com/gofiber/fiber/v2"
 
 func Authlogin(c *fiber.Ctx) error {
+	username := c.FormValue("username")
+	password := c.FormValue("password")
+
+	// Check if the credentials are valid
+	user, exists := users[username]
+	var checkPassword string
+	if exists {
+		checkPassword = user.Password
+	} else {
+		checkPassword = emptyHashString
+	}
+
+	if bcrypt.CompareHashAndPassword([]byte(checkPassword), []byte(password)) != nil {
+		// Authentication failed
+		csrfToken, ok := c.Locals("csrf").(string)
+		if !ok {
+			return c.SendStatus(fiber.StatusInternalServerError)
+		}
+
+		return c.Render("login", fiber.Map{
+			"Title": "Login",
+			"csrf":  csrfToken,
+			"error": "Invalid credentials",
+		})
+	}
+
 	return c.SendString("You are login 👋!")
 }
--- routers/routes.go
+++ routers/routes.go
@@ -1,6 +1,7 @@
 package routers
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/donairl/gofiber-dontemplate/handlers"
@@ -8,8 +9,8 @@ import (
 	"github.com/gofiber/fiber/v2/middleware/csrf"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/gofiber/fiber/v2/middleware/monitor"
+	"github.com/gofiber/fiber/v2/middleware/session"
 	"github.com/gofiber/template/html/v2"
-	"github.com/gofiber/utils/v2"
 )
 
 func New() *fiber.App {
@@ -23,13 +24,56 @@ func New() *fiber.App {
 	app := fiber.New(fiber.Config{
 		Views: engine,
 	})
-	app.Use(csrf.New(csrf.Config{
-		KeyLookup:      "header:X-Csrf-Token",
-		CookieName:     "csrf_",
+
+	// Initialize a session store
+	sessConfig := session.Config{
+		Expiration:     30 * time.Minute,        // Expire sessions after 30 minutes of inactivity
+		KeyLookup:      "cookie:__Host-session", // Recommended to use the __Host- prefix when serving the app over TLS
+		CookieSecure:   true,
+		CookieHTTPOnly: true,
 		CookieSameSite: "Lax",
-		Expiration:     1 * time.Hour,
-		KeyGenerator:   utils.UUIDv4,
-	}))
+	}
+	store := session.New(sessConfig)
+
+	csrfErrorHandler := func(c *fiber.Ctx, err error) error {
+		// Log the error so we can track who is trying to perform CSRF attacks
+		// customize this to your needs
+		fmt.Printf("CSRF Error: %v Request: %v From: %v\n", err, c.OriginalURL(), c.IP())
+
+		// check accepted content types
+		switch c.Accepts("html", "json") {
+		case "json":
+			// Return a 403 Forbidden response for JSON requests
+			return c.Status(fiber.StatusForbidden).JSON(fiber.Map{
+				"error": "403 Forbidden",
+			})
+		case "html":
+			// Return a 403 Forbidden response for HTML requests
+			return c.Status(fiber.StatusForbidden).Render("error", fiber.Map{
+				"Title":     "Error",
+				"Error":     "403 Forbidden",
+				"ErrorCode": "403",
+			})
+		default:
+			// Return a 403 Forbidden response for all other requests
+			return c.Status(fiber.StatusForbidden).SendString("403 Forbidden")
+		}
+	}
+	// Configure the CSRF middleware
+	csrfConfig := csrf.Config{
+		Session:        store,
+		KeyLookup:      "form:csrf",   // In this example, we will be using a hidden input field to store the CSRF token
+		CookieName:     "__Host-csrf", // Recommended to use the __Host- prefix when serving the app over TLS
+		CookieSameSite: "Lax",         // Recommended to set this to Lax or Strict
+		CookieSecure:   true,          // Recommended to set to true when serving the app over TLS
+		CookieHTTPOnly: true,          // Recommended, otherwise if using JS framework recomend: false and KeyLookup: "header:X-CSRF-Token"
+		ContextKey:     "csrf",
+		ErrorHandler:   csrfErrorHandler,
+		Expiration:     30 * time.Minute,
+	}
+
+	app.Use(csrfConfig)
+
 	app.Use(logger.New(logger.Config{
 		Format: "[${ip}]:${port} ${status} - ${method} ${path}\n",
 	}))
@@ -42,8 +86,11 @@ func New() *fiber.App {
 
 	app.Get("/login", func(c *fiber.Ctx) error {
 		// Render index
+		csrfToken, ok := c.Locals("csrf").(string)
+
 		return c.Render("login", fiber.Map{
 			"Title": "Login Page",
+			"csrf":  csrfToken,
 		}, "layouts/main")
 	})
 
